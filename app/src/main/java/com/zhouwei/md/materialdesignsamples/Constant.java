6e85850656fe1e9d4943a41ae38439d321c836eb
zyh2017 <121583054@qq.com>
2017-08-03 15:29:47 +0800

    代码分析，添加注解

---
+++ app/src/main/java/com/zhouwei/md/materialdesignsamples/Constant.java
@@ -0,0 +1,10 @@
+package com.zhouwei.md.materialdesignsamples;
+
+/**
+ * 作者：张玉辉
+ * 时间：2017/8/3.
+ */
+
+public class Constant {
+    public static final String TAG = "tag";
+}
--- app/src/main/java/com/zhouwei/md/materialdesignsamples/behavoir/FloatingHeaderTitleBehavior.java
+++ app/src/main/java/com/zhouwei/md/materialdesignsamples/behavoir/FloatingHeaderTitleBehavior.java
@@ -4,8 +4,10 @@ import android.animation.ArgbEvaluator;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
+import com.zhouwei.md.materialdesignsamples.Constant;
 import com.zhouwei.md.materialdesignsamples.MaterialDesignSimpleApplication;
 import com.zhouwei.md.materialdesignsamples.R;
 
@@ -33,18 +35,38 @@ public class FloatingHeaderTitleBehavior extends CoordinatorLayout.Behavior<View
         mMargin = context.getResources().getDimensionPixelOffset(R.dimen.title_margin_left);
     }
 
+    /**
+     * 生成Behavior后第一件事就是确定依赖关系,重写Behavior的这个方法来确定你依赖哪些View。
+     * 确定你是否依赖于这个View，CoordinatorLayout会将自己所有View遍历判断，
+     * 如果确定依赖，这个方法很重要，当所依赖的View变动时会回调这个方法。
+     * @param parent
+     * @param child  指应用behavior的View
+     * @param dependency  担任触发behavior的View，并与child进行互动
+     * @return
+     */
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        Log.d(Constant.TAG,"layoutDependsOn");
         return isDependent(dependency);
     }
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        //触发behavior的控件的移动距离
+        float dependencyTranslationY = dependency.getTranslationY();
+        //触发behavior的控件的高度：400
+        int dependencyHeight = dependency.getHeight();
+        //收缩后的高度:100
+        int collapsedHeight = getCollapsedHeight();
 
-        float progress = 1.0f - Math.abs(dependency.getTranslationY() / (dependency.getHeight() - getCollapsedHeight()));
+        // 1 - 触发behavior的控件的移动距离与可移动距离的百分比
+        float progress = 1.0f - Math.abs( dependencyTranslationY/ (dependencyHeight - collapsedHeight));
 
         float translationY = (mTitleInitY - mTitleCollapsedHeight) * progress;
 
+        Log.d(Constant.TAG,"dependencyTranslationY:"+dependencyTranslationY+",dependencyHeight:"+dependencyHeight+
+                ",collapsedHeight:"+collapsedHeight+",progress"+progress+",translationY:"+translationY);
+        //应用behavior的View的在Y轴方向上移动的距离
         child.setTranslationY(translationY);
 
         // background change
